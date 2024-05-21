0bcb4cc8dae6a4dd0629e94a8391991f808f1189
AJD254 <jontexjesus@gmail.com>
2024-05-21 18:03:59 +0300

    typescript setup using txt

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const controler_1 = require("./controler");
+console.log((0, controler_1.sum)(20, 300));
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+
+
+
+import {sum} from './controler';
+console.log(sum(20,300))
